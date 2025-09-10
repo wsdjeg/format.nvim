8f8e20af66801a237250ed3387ef162a025df7db
Eric Wong <wsdjeg@outlook.com>
2025-09-10 23:51:26 +0800

    feat(cmd): add FormatWrite command

--- plugin/format.lua
+++ plugin/format.lua
@@ -18,3 +18,28 @@ end, {
   end,
 })
 
+vim.api.nvim_create_user_command('FormatWrite', function(opts)
+  require('format').format(opts.bang, opts.args, opts.line1, opts.line2, {
+    hooks = {
+      post = function(buf)
+        vim.api.nvim_buf_call(buf, vim.cmd.write)
+      end,
+    },
+  })
+end, {
+  nargs = '*',
+  range = '%',
+  bang = true,
+  bar = true,
+  complete = function(_, line)
+    local ft = vim.o.filetype
+    local l = vim.split(line, '%s+')
+    local ok, default = pcall(require, 'format.ft.' .. ft)
+    if ok then
+      return vim.tbl_filter(function(val)
+        return vim.startswith(val, l[#l])
+      end, default.enabled())
+    else
+    end
+  end,
+})
